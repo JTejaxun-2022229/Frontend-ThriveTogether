1fb8c06b9bc3378ddff1e45c286cb3c4aeece323
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-07-22 09:17:13 -0600

    new routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,10 @@
 import { Foro } from "./Foro/foro";
+import { AddForum } from "./Foro/addForum";
+import { ForumList } from "./Foro/forumList";
 
 const routes = [
+    {path: '/forums/', element: <ForumList /> },
+    {path: '/forum/', element: <AddForum /> },
     {path: '/forum/:id', element: <Foro /> },
 ]
 
